87548120d1ba933bd8f77db64b72ba91ae1cea0e
Luca Palmarucci <lpalmarucci@gmail.com>
2024-10-24 21:58:39 +0200

    setup next-auth

---
+++ auth.ts
@@ -0,0 +1,22 @@
+import { AuthOptions, getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions: AuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.SECRET,
+};
+
+/**
+ * Helper function to get the session on the server without having to import the authOptions object every single time
+ * @returns The session object or null
+ */
+const getSession = () => getServerSession(authOptions);
+
+export { authOptions, getSession };
---
+++ components/AuthButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function AuthButton() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        {session.user?.name}
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      Not signed in
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
+}
