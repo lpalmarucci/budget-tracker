b813e0b01f388cbe77a07de64d8c06d35bbbc119
Luca Palmarucci <lpalmarucci@gmail.com>
2025-02-24 19:59:01 +0100

    (fix): shortcodes param of em-emoji might be null

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,8 @@
 declare namespace JSX {
-    interface IntrinsicElements {
-        'em-emoji': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-            shortcodes?: string;
-            size?: string;
-        };
-    }
+  interface IntrinsicElements {
+    "em-emoji": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      shortcodes: string | null;
+      size?: string;
+    };
+  }
 }
