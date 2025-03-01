ad156ad01faac7ce17a5d245180eced027bf0742
Luca Palmarucci <lpalmarucci@gmail.com>
2025-03-01 20:30:33 +0100

    (fix): useHistory type

--- features/overview/history.api.ts
+++ features/overview/history.api.ts
@@ -6,7 +6,7 @@ import { OVERVIEW_QUERY_KEY } from "@/features/overview/overview.api";
 export const HISTORY_QUERY_KEY = "HISTORY";
 
 export function useHistory(timeframe: TimeframeHistoryType, period: PeriodHistoryType) {
-  return useQuery<HistoryData>({
+  return useQuery<HistoryData[]>({
     queryKey: [OVERVIEW_QUERY_KEY, HISTORY_QUERY_KEY, timeframe, period],
     queryFn: () =>
       fetch(`/api/history?timeframe=${timeframe}&year=${period.year}&month=${period.month}`).then((res) => res.json()),
